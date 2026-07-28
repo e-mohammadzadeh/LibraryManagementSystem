c1504cfe1a803b82296a42c8b00a0b38c1d373d1
Erfan Mohammadzadeh <er.mohammadzadeh@gmail.com>
2026-07-28 20:00:48 +0330

    Complete fine repository

--- LibraryManagementSystem.Domain/Interfaces/IFineRepository.cs
+++ LibraryManagementSystem.Domain/Interfaces/IFineRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace LibraryManagementSystem.Domain.Interfaces;
+﻿using LibraryManagementSystem.Domain.Entities;
+
+namespace LibraryManagementSystem.Domain.Interfaces;
 
 public interface IFineRepository
 {
-	
+	void Add(Fine fine);
+	Fine? FindById(int findId);
+	IReadOnlyList<Fine> GetAll();
+	IReadOnlyList<Fine> GetByUserId(int userId);
+	IReadOnlyList<Fine> GetUnpaidByUserId(int userId);
+	IReadOnlyList<Fine> GetUnpaidByLoanId(int loanId);
+	bool HasUnpaidFines(int userId);
+	decimal GetTotalUnpaidAmound(int userId);
+	void Remove(Fine fine);
+	void Update(Fine fine);
+	int Count();
 }
--- LibraryManagementSystem.Infrastructure/Repositories/InMemory/InMemoryFineRepository.cs
+++ LibraryManagementSystem.Infrastructure/Repositories/InMemory/InMemoryFineRepository.cs
@@ -1,6 +1,76 @@
-﻿namespace LibraryManagementSystem.Infrastructure.Repositories.InMemory;
+﻿using LibraryManagementSystem.Domain.Entities;
+using LibraryManagementSystem.Domain.Enums;
+using LibraryManagementSystem.Domain.Interfaces;
 
-public class InMemoryFineRepository
+namespace LibraryManagementSystem.Infrastructure.Repositories.InMemory;
+
+public class InMemoryFineRepository:IFineRepository
 {
-	
+	private readonly List<Fine> _fines = [];
+
+	public void Add(Fine fine)
+	{
+		_fines.Add(fine);
+	}
+
+
+	public Fine? FindById(int findId)
+	{
+		return _fines.FirstOrDefault(f => f.FineId == findId);
+	}
+
+
+	public IReadOnlyList<Fine> GetAll()
+	{
+		return _fines.ToList().AsReadOnly();
+	}
+
+
+	public IReadOnlyList<Fine> GetByUserId(int userId)
+	{
+		return _fines.Where(f => f.Loan.UserId == userId).ToList().AsReadOnly();
+	}
+
+
+	public IReadOnlyList<Fine> GetUnpaidByUserId(int userId)
+	{
+		return _fines.Where(f => f.Loan.UserId == userId && f.Status == FineStatus.Unpaid).ToList().AsReadOnly();
+	}
+
+
+	public IReadOnlyList<Fine> GetUnpaidByLoanId(int loanId)
+	{
+		return _fines.Where(f => f.LoanId == loanId && f.Status == FineStatus.Unpaid).ToList().AsReadOnly();
+	}
+
+
+	public bool HasUnpaidFines(int userId)
+	{
+		return _fines.Any(f => f.Loan.UserId == userId && f.Status == FineStatus.Unpaid);
+	}
+
+
+	public decimal GetTotalUnpaidAmound(int userId)
+	{
+		return _fines.Where(f => f.Loan.UserId == userId && f.Status == FineStatus.Unpaid).Sum(f => f.Amount);
+	}
+
+
+	public void Remove(Fine fine)
+	{
+		_fines.Remove(fine);
+	}
+
+
+	public void Update(Fine fine)
+	{
+		// In-memory implementation:
+		// Fine is already tracked by reference.
+	}
+
+
+	public int Count()
+	{
+		return _fines.Count;
+	}
 }
